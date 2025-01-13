7695f56bde8e091af8d70c503c39082f80f00e74
usysgdejyrdh <delete7778@gmail.com>
2025-01-13 19:01:45 +0530

    soul.py

--- soul.py
+++ soul.py
@@ -15,7 +15,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 loop = asyncio.get_event_loop()
 
 # Bot Configuration: Set with Authority
-TOKEN = '7457933947:AAHqGcmvzQNGsm-HDKQ-ws1d3q6Gxim9KHA'
+TOKEN = '8129833857:AAFxeCcVZvuvAUDUJwBbFEf51r5Qb0rFDsE'
 ADMIN_USER_ID = 5676671574
 MONGO_URI = 'mongodb+srv://sharp:sharp@sharpx.x82gx.mongodb.net/?retryWrites=true&w=majority&appName=SharpX'
 USERNAME = "@LION1684"  # Immutable username for maximum security
