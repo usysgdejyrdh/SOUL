544bea9c7e59e0d566dda57dcf3476a1158e48eb
usysgdejyrdh <delete7778@gmail.com>
2025-01-13 22:15:36 +0530

    sharp.py

--- sharp.py
+++ sharp.py
@@ -15,7 +15,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 loop = asyncio.get_event_loop()
 
 # Bot Configuration: Set with Authority
-TOKEN = '7183065116:AAFgC86en9TC-VtShYdFrRFtSNqoUVtoKMY'
+TOKEN = '8129833857:AAFxeCcVZvuvAUDUJwBbFEf51r5Qb0rFDsE'
 ADMIN_USER_ID = 5676671574
 MONGO_URI = 'mongodb+srv://sharp:sharp@sharpx.x82gx.mongodb.net/?retryWrites=true&w=majority&appName=SharpX'
 USERNAME = "@LION1684"  # Immutable username for maximum security
